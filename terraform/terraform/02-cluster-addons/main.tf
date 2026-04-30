6abfadfa3d23068e92b373afbc195e17234b08fd
uday akula <udaykiran0117@gmail.com>
2026-04-30 14:10:43 -0400

    Fix Terraform formatting

--- terraform/terraform/02-cluster-addons/main.tf
+++ terraform/terraform/02-cluster-addons/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "nginx_ingress" {
-  name       = "nginx-ingress"
-  namespace  = "ingress-nginx"
+  name      = "nginx-ingress"
+  namespace = "ingress-nginx"
 
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
@@ -10,8 +10,8 @@ resource "helm_release" "nginx_ingress" {
 
 
 resource "helm_release" "monitoring" {
-  name       = "monitoring"
-  namespace  = "monitoring"
+  name      = "monitoring"
+  namespace = "monitoring"
 
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
@@ -20,8 +20,8 @@ resource "helm_release" "monitoring" {
 }
 
 resource "helm_release" "strimzi" {
-  name       = "strimzi"
-  namespace  = "kafka"
+  name      = "strimzi"
+  namespace = "kafka"
 
   repository = "https://strimzi.io/charts/"
   chart      = "strimzi-kafka-operator"
@@ -31,16 +31,16 @@ resource "helm_release" "strimzi" {
 
 
 resource "helm_release" "csi_driver" {
-  name       = "csi-secrets-store"
-  namespace  = "kube-system"
+  name      = "csi-secrets-store"
+  namespace = "kube-system"
 
   repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
   chart      = "secrets-store-csi-driver"
 }
 
 resource "helm_release" "azure_kv_provider" {
-  name       = "azure-keyvault-provider"
-  namespace  = "kube-system"
+  name      = "azure-keyvault-provider"
+  namespace = "kube-system"
 
   repository = "https://azure.github.io/secrets-store-csi-driver-provider-azure/charts"
   chart      = "csi-secrets-store-provider-azure"
