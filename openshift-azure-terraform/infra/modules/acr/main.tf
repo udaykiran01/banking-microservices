eed66b4a56a767d2fd451e373389e3814d29fdb3
uday akula <udaykiran0117@gmail.com>
2026-06-21 22:45:51 -0400

    test terraform pipeline

--- openshift-azure-terraform/infra/modules/acr/main.tf
+++ openshift-azure-terraform/infra/modules/acr/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_container_registry" "this" {
   name                = var.container_registry_name
   resource_group_name = var.resource_group_name
-  #hello mastaru
+  #pipeline testing
   location            = var.location
   sku                 = "Basic"
   admin_enabled       = false
